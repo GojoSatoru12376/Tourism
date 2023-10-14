67de426cd597746ac751d77a72f3a97f15711629
GojoSatoru12376 <it.a.32.dishaa.goswami@gmail.com>
2023-10-14 20:14:14 +0530

    Added the attraction tourist place and their info

---
+++ src/pages/touristPlace/TouristPlace.jsx
@@ -0,0 +1,24 @@
+import Attraction from '../../components/attraction/Attraction';
+import Attraction1 from '../../components/attraction/Attraction1';
+import Footer from '../../components/footer/Footer';
+import Header from '../../components/header/Header';
+import MailList from '../../components/mailList/MailList';
+import Navbar from '../../components/navbar/Navbar';
+import './touristPlace.css';
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+      <div className="homeContainer">
+        <Attraction />
+        <Attraction1 />
+        <MailList />
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
